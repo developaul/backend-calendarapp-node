c7aa0ae3c8dbb623652284abbd306b89bd542dae
Paul Chávez <paulchavezromero20@gmail.com>
2021-03-05 15:15:23 -0500

    User model added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,47 @@
 const { request, response } = require('express');
+const bcrypt = require('bcryptjs');
 
-const createUser = (req = request, res = response) => {
+const User = require('../models/User');
 
-  const { name, email, password } = req.body;
+const createUser = async (req = request, res = response) => {
 
-  res.status(201).json({
-    ok: true,
-    msg: 'Usuario registrado',
-    name,
-    email,
-    password
-  });
+  const { email, password } = req.body;
+
+  try {
+
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Un usuario existe con ese correo'
+      });
+    }
+
+    user = new User(req.body);
+
+    // Encriptar contraseña
+    const salt = bcrypt.genSaltSync(10);
+    user.password = bcrypt.hashSync(password, salt);
+
+    await user.save();
+
+    res.status(201).json({
+      ok: true,
+      uid: user.id,
+      name: user.name
+    });
+
+  } catch (err) {
+
+    console.log(err);
+
+    res.status(500).json({
+      ok: false,
+      msg: 'Por favor hable con el administrador'
+    });
+
+  }
 
 }
 
